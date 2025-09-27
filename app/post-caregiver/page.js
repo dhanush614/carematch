7a43cf8d84de60cd0957ac46f4598a1bc3223d3f
Dhanush <dhanushkumar614@gmail.com>
2025-09-27 03:31:12 -0400

    fix: Add Image import to post-caregiver page

--- app/post-caregiver/page.js
+++ app/post-caregiver/page.js
@@ -1,6 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
+import Image from "next/image"
 import { supabase } from "../../lib/supabaseClient"
 import { useAuth } from "../context/auth"
 import { useRouter } from "next/navigation"
